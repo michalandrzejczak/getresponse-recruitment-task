87626257ded53b7ee8046808e3cd41d6dc235b4e
Michal Andrzejczak <michal.andrzejcz@gmail.com>
2019-12-02 19:45:08 +0100

    Updates

---
+++ precache-manifest.013e479ac86df1b1edc9ea7239385dad.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd3a3b0c0a8af692761f1b47ff0f18f4",
+    "url": "/getresponse-recruitment-task/index.html"
+  },
+  {
+    "revision": "5fd1d3d29affb91e00d7",
+    "url": "/getresponse-recruitment-task/static/css/main.e30c35fa.chunk.css"
+  },
+  {
+    "revision": "2165cf84e1e6da96243c",
+    "url": "/getresponse-recruitment-task/static/js/2.8de82bc4.chunk.js"
+  },
+  {
+    "revision": "5fd1d3d29affb91e00d7",
+    "url": "/getresponse-recruitment-task/static/js/main.de29b6ec.chunk.js"
+  },
+  {
+    "revision": "99840508cf784e9f1d4f",
+    "url": "/getresponse-recruitment-task/static/js/runtime-main.19514152.js"
+  },
+  {
+    "revision": "8613d226aee08ea485d37af67935b9d5",
+    "url": "/getresponse-recruitment-task/static/media/ARIALROUNDEDMT.8613d226.woff"
+  },
+  {
+    "revision": "f0becb33c0d8b03ddc93c5e94c8f1048",
+    "url": "/getresponse-recruitment-task/static/media/HelveticaNeueLTStd45Light_22520.f0becb33.woff"
+  },
+  {
+    "revision": "28c91a2c0eed51c62c957a5e9efa9201",
+    "url": "/getresponse-recruitment-task/static/media/HelveticaNeueLTStd55Roman_22526.28c91a2c.woff"
+  },
+  {
+    "revision": "933fca8fd6521571a22e2db87cadcd79",
+    "url": "/getresponse-recruitment-task/static/media/HelveticaNeueLTStd75Bold_22539.933fca8f.woff"
+  },
+  {
+    "revision": "c62cceb8297897694d86162324771540",
+    "url": "/getresponse-recruitment-task/static/media/HelveticaNeueLTStdMd 1.c62cceb8.woff"
+  },
+  {
+    "revision": "cc9849ec111e9a3f917c6c59fd242161",
+    "url": "/getresponse-recruitment-task/static/media/bode-shake.cc9849ec.png"
+  },
+  {
+    "revision": "dbbb84f202a2edd2f55539ccf60e023e",
+    "url": "/getresponse-recruitment-task/static/media/protein-steak-disabled.dbbb84f2.png"
+  },
+  {
+    "revision": "03bce23baa648b6644526afe8e58c5e0",
+    "url": "/getresponse-recruitment-task/static/media/protein-steak.03bce23b.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5c1b0441b7566b456ac98225157ec1d3.js"
+  "/getresponse-recruitment-task/precache-manifest.013e479ac86df1b1edc9ea7239385dad.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/getresponse-recruitment-task/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
